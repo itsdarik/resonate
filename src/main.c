a4a1edd427d0c89817d40b6c1a76da3a42c2eb9d
Darik Harter <darik.harter@gmail.com>
2024-10-11 15:25:15 -0600

    main.c: Create DTLS context and connect

--- src/main.c
+++ src/main.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include "hue_dtls_client.h"
+
 int main(void)
 {
+    hue_dtls_context *context = hue_dtls_context_create();
+    if (!context)
+    {
+        fprintf(stderr, "Failed to create DTLS context\n");
+        return 1;
+    }
+
+    if (hue_dtls_connect(context))
+    {
+        fprintf(stderr, "Failed to connect to Hue bridge\n");
+        hue_dtls_context_free(context);
+        return 1;
+    }
+
+    hue_dtls_context_free(context);
     return 0;
 }
