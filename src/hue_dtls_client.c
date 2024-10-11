9a6d44ff7805718efb018aa8a78f7655f2259520
Darik Harter <darik.harter@gmail.com>
2024-10-11 17:46:29 -0600

    Implement socket connect and error messages

--- src/hue_dtls_client.c
+++ src/hue_dtls_client.c
@@ -1,10 +1,53 @@
 #include "hue_dtls_client.h"
-#include <stddef.h> // NULL
-#include <stdio.h>  // sscanf
+#include <netinet/in.h> // struct sockaddr_in
+#include <stddef.h> // NULL, size_t
+#include <stdio.h>  // perror, sscanf
 #include <stdlib.h> // getenv, malloc, free
 #include <string.h> // strncpy, strlen
+#include <sys/socket.h> // socket, connect
+#include <unistd.h> // close
 
+#define MAX_PORT_NUMBER 65535
 #define PSK_HEX_EXPECTED_LEN 32
+
+static int socket_connect(hue_dtls_context *context, const char *bridge_ip, const char *bridge_port) {
+  if (!context) {
+    fprintf(stderr, "context is null\n");
+    return -1;
+  }
+
+  const int port = atoi(bridge_port);
+  if (port <= 0 || port > MAX_PORT_NUMBER) {
+    fprintf(stderr, "Invalid port number: %s\n", bridge_port);
+    return -1;
+  }
+
+  context->socket = socket(AF_INET, SOCK_DGRAM, 0);
+  if (context->socket < 0) {
+    perror("socket");
+    context->socket = -1;
+    return -1;
+  }
+
+  struct sockaddr_in addr = {0};
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  if (inet_pton(AF_INET, bridge_ip, &addr.sin_addr) != 1) {
+    perror("inet_pton");
+    close(context->socket);
+    context->socket = -1;
+    return -1;
+  }
+
+  if (connect(context->socket, (struct sockaddr *)&addr, sizeof(addr))) {
+    perror("connect");
+    close(context->socket);
+    context->socket = -1;
+    return -1;
+  }
+
+  return 0;
+}
 
 static unsigned int psk_client_callback(SSL *ssl, const char *hint,
                                         char *identity,
@@ -15,11 +58,13 @@ static unsigned int psk_client_callback(SSL *ssl, const char *hint,
   const char *psk_hex = getenv("HUE_CLIENTKEY");
 
   if (!psk_identity || !psk_hex) {
+    fprintf(stderr, "HUE_APPLICATION_ID or HUE_CLIENTKEY not set\n");
     return 0;
   }
 
   // Set the PSK identity.
   if (strlen(psk_identity) >= max_identity_len) {
+    fprintf(stderr, "PSK identity is too long\n");
     return 0;
   }
 
@@ -28,11 +73,14 @@ static unsigned int psk_client_callback(SSL *ssl, const char *hint,
   // Set the PSK key.
   const size_t psk_hex_len = strlen(psk_hex);
   if (psk_hex_len != PSK_HEX_EXPECTED_LEN) {
+    fprintf(stderr, "PSK hex length (%zu) is not the expected length (%d)\n",
+            psk_hex_len, PSK_HEX_EXPECTED_LEN);
     return 0;
   }
 
   // Convert the PSK key from hex to binary.
   if (psk_hex_len / 2 > max_psk_len) {
+    fprintf(stderr, "PSK hex is too long\n");
     return 0;
   }
 
@@ -46,11 +94,13 @@ static unsigned int psk_client_callback(SSL *ssl, const char *hint,
 hue_dtls_context *hue_dtls_context_create(void) {
   hue_dtls_context *context = malloc(sizeof(hue_dtls_context));
   if (!context) {
+    perror("malloc");
     return NULL;
   }
 
   context->ssl_ctx = SSL_CTX_new(DTLS_client_method());
   if (!context->ssl_ctx) {
+    fprintf(stderr, "SSL_CTX_new() failed\n");
     free(context);
     return NULL;
   }
@@ -78,7 +128,51 @@ void hue_dtls_context_free(hue_dtls_context *context) {
   }
 }
 
-int hue_dtls_connect(hue_dtls_context *context) { return 0; }
+int hue_dtls_connect(hue_dtls_context *context, const char *bridge_ip, const char *bridge_port) {
+  if (!context || !context->ssl_ctx) {
+    fprintf(stderr, "context or context->ssl_ctx is null\n");
+    return -1;
+  }
+
+  const int port = atoi(bridge_port);
+  if (port <= 0 || port > MAX_PORT_NUMBER) {
+    fprintf(stderr, "Invalid port number: %s\n", bridge_port);
+    return -1;
+  }
+
+  context->ssl = SSL_new(context->ssl_ctx);
+  if (!context->ssl) {
+    fprintf(stderr, "SSL_new() failed\n");
+    return -1;
+  }
+
+  if (socket_connect(context, bridge_ip, bridge_port)) {
+    fprintf(stderr, "socket_connect() failed\n");
+    SSL_free(context->ssl);
+    context->ssl = NULL;
+    return -1;
+  }
+
+  if (SSL_set_fd(context->ssl, context->socket) != 1) {
+    fprintf(stderr, "SSL_set_fd() failed\n");
+    SSL_free(context->ssl);
+    context->ssl = NULL;
+    close(context->socket);
+    context->socket = -1;
+    return -1;
+  }
+
+  if (SSL_connect(context->ssl) != 1) {
+    fprintf(stderr, "SSL_connect() failed\n");
+    SSL_free(context->ssl);
+    context->ssl = NULL;
+    close(context->socket);
+    context->socket = -1;
+    return -1;
+  }
+
+  return 0;
+}
 
 int hue_dtls_send_message(hue_dtls_context *context,
                           const hue_stream_message *message) {
