8f5b382b1d34dc07784d1985f1bebaa1486bd5b5
Darik Harter <darik.harter@gmail.com>
2024-10-12 11:15:16 -0600

    Refactor hue_dtls_client.c

--- src/hue_dtls_client.c
+++ src/hue_dtls_client.c
@@ -1,15 +1,16 @@
 #include "hue_dtls_client.h"
+
+#include <mbedtls/timing.h>
+
 #include <errno.h>  // errno
 #include <stddef.h> // NULL, size_t
 #include <stdio.h>  // perror, sscanf
 #include <stdlib.h> // getenv, malloc, free
 #include <string.h> // strncpy, strlen
 
-#include <mbedtls/debug.h>
-#include <mbedtls/timing.h>
-
-#define HUE_BRIDGE_DTLS_CIPHER "TLS-PSK-WITH-AES-128-GCM-SHA256"
+#define HUE_BRIDGE_DTLS_CIPHER MBEDTLS_TLS_PSK_WITH_AES_128_GCM_SHA256
 #define HUE_BRIDGE_DTLS_PORT 2100
+
 #define PSK_HEX_EXPECTED_LEN 32
 
 typedef struct {
@@ -44,7 +45,7 @@ static int udp_socket_connect(hue_dtls_context *context,
   return 0;
 }
 
-static int setup_psk(mbedtls_ssl_config *conf) {
+static int set_psk(mbedtls_ssl_config *conf) {
   const char *psk_identity = getenv("HUE_APPLICATION_ID");
   const char *psk_hex = getenv("HUE_CLIENTKEY");
 
@@ -53,25 +54,24 @@ static int setup_psk(mbedtls_ssl_config *conf) {
     return -1;
   }
 
-  unsigned char psk[PSK_HEX_EXPECTED_LEN / 2];
   const size_t psk_hex_len = strlen(psk_hex);
-
   if (psk_hex_len != PSK_HEX_EXPECTED_LEN) {
     fprintf(stderr, "PSK hex length (%zu) is not the expected length (%d)\n",
             psk_hex_len, PSK_HEX_EXPECTED_LEN);
     return -1;
   }
 
+  unsigned char psk[psk_hex_len / 2];
   for (size_t i = 0; i < psk_hex_len / 2; i++) {
     if (sscanf(psk_hex + 2 * i, "%2hhx", &psk[i]) != 1) {
-      fprintf(stderr, "Failed to parse PSK hex string\n");
+      perror("sscanf");
       return -1;
     }
   }
 
   if (mbedtls_ssl_conf_psk(conf, psk, sizeof(psk),
                            (const unsigned char *)psk_identity,
-                           strlen(psk_identity)) != 0) {
+                           strlen(psk_identity))) {
     fprintf(stderr, "mbedtls_ssl_conf_psk() failed\n");
     return -1;
   }
@@ -86,50 +86,77 @@ hue_dtls_context *hue_dtls_context_create(void) {
     return NULL;
   }
 
+  // Initialize Mbed TLS structures.
+  mbedtls_net_init(&context->server_fd);
   mbedtls_ssl_init(&context->ssl);
   mbedtls_ssl_config_init(&context->conf);
   mbedtls_ctr_drbg_init(&context->ctr_drbg);
   mbedtls_entropy_init(&context->entropy);
 
+  // Seed the random number generator.
+  const char *pers = "hue_dtls_client";
   if (mbedtls_ctr_drbg_seed(&context->ctr_drbg, mbedtls_entropy_func,
-                            &context->entropy, NULL, 0) != 0) {
+                            &context->entropy, (const unsigned char *)pers,
+                            strlen(pers))) {
     fprintf(stderr, "mbedtls_ctr_drbg_seed() failed\n");
-    free(context);
-    return NULL;
+    goto exit;
   }
 
+  // Set the SSL configuration.
   if (mbedtls_ssl_config_defaults(&context->conf, MBEDTLS_SSL_IS_CLIENT,
                                   MBEDTLS_SSL_TRANSPORT_DATAGRAM,
-                                  MBEDTLS_SSL_PRESET_DEFAULT) != 0) {
+                                  MBEDTLS_SSL_PRESET_DEFAULT)) {
     fprintf(stderr, "mbedtls_ssl_config_defaults() failed\n");
-    free(context);
-    return NULL;
+    goto exit;
   }
 
   mbedtls_ssl_conf_rng(&context->conf, mbedtls_ctr_drbg_random,
                        &context->ctr_drbg);
 
-  if (setup_psk(&context->conf) != 0) {
-    free(context);
-    return NULL;
+  // Set the pre-shared key (PSK).
+  if (set_psk(&context->conf)) {
+    fprintf(stderr, "set_psk() failed\n");
+    goto exit;
   }
 
-  if (mbedtls_ssl_setup(&context->ssl, &context->conf) != 0) {
+  // Setup the SSL context.
+  if (mbedtls_ssl_setup(&context->ssl, &context->conf)) {
     fprintf(stderr, "mbedtls_ssl_setup() failed\n");
-    free(context);
-    return NULL;
+    goto exit;
   }
+
+  // Limit ciphersuites to the one used by the Hue bridge.
+  context->ciphersuites[0] = HUE_BRIDGE_DTLS_CIPHER;
+  context->ciphersuites[1] = 0;
 
   return context;
+
+exit:
+  mbedtls_net_free(&context->server_fd);
+  mbedtls_ssl_free(&context->ssl);
+  mbedtls_ssl_config_free(&context->conf);
+  mbedtls_ctr_drbg_free(&context->ctr_drbg);
+  mbedtls_entropy_free(&context->entropy);
+  free(context);
+  return NULL;
 }
 
 void hue_dtls_context_free(hue_dtls_context *context) {
   if (context) {
+    // Close the connection. Errors are ok since the connection might already be
+    // closed.
+    int ret = 0;
+    do {
+      ret = mbedtls_ssl_close_notify(&context->ssl);
+    } while (ret == MBEDTLS_ERR_SSL_WANT_WRITE);
+
+    // Free Mbed TLS structures.
+    mbedtls_net_free(&context->server_fd);
     mbedtls_ssl_free(&context->ssl);
     mbedtls_ssl_config_free(&context->conf);
     mbedtls_ctr_drbg_free(&context->ctr_drbg);
     mbedtls_entropy_free(&context->entropy);
-    mbedtls_net_free(&context->net);
+
     free(context);
   }
 }
