d767a360274a2cc44469314e89bbeebea54d5ef0
Darik Harter <darik.harter@gmail.com>
2024-10-12 17:13:23 -0600

    Refactor main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "hue_dtls_client.h"
 #include <pthread.h>
 #include <stdbool.h> // bool
-#include <stdio.h>   // fprintf, printf
+#include <stdio.h>   // fprintf, printf, scanf
 #include <stdlib.h>  // free
 #include <string.h>  // memcpy
 #include <time.h>    // nanosleep
@@ -10,16 +10,13 @@
 #define CHANNEL_COUNT 10
 #define ENTERTAINMENT_CONFIG_ID "2d4cb563-4244-4bfc-9bb2-f5a08068df84"
 
+#define FRAMES_PER_SECOND 60
+#define NANOSECONDS_PER_FRAME (1000000000L / FRAMES_PER_SECOND)
+
 bool streaming = true;
 
 pthread_mutex_t curent_frame_mutex = {0};
-hue_stream_message_data current_frame[CHANNEL_COUNT] = {
-    {0, {0x0000, 0xFFFF, 0xFFFF}}, {1, {0x0000, 0xFFFF, 0xFFFF}},
-    {2, {0x0000, 0xFFFF, 0xFFFF}}, {3, {0x0000, 0xFFFF, 0xFFFF}},
-    {4, {0x0000, 0xFFFF, 0xFFFF}}, {5, {0x0000, 0xFFFF, 0xFFFF}},
-    {6, {0x0000, 0xFFFF, 0xFFFF}}, {7, {0x0000, 0xFFFF, 0xFFFF}},
-    {8, {0x0000, 0xFFFF, 0xFFFF}}, {9, {0x0000, 0xFFFF, 0xFFFF}},
-};
+hue_stream_message_data current_frame[CHANNEL_COUNT] = {0};
 
 typedef struct stream_thread_args stream_thread_args;
 struct stream_thread_args {
@@ -50,12 +47,59 @@ void *stream(void *arg) {
 
     free(message);
 
-    // Stream between 50 and 60 frames per second.
-    struct timespec ts = {.tv_sec = 0, .tv_nsec = 17000000L};
+    // Stream at the specified frame rate.
+    struct timespec ts = {.tv_sec = 0, .tv_nsec = NANOSECONDS_PER_FRAME};
     nanosleep(&ts, NULL);
   }
 
   return NULL;
+}
+
+static hue_dtls_context *connect_to_bridge(const char *bridge_ip) {
+  if (!bridge_ip) {
+    fprintf(stderr, "bridge_ip is null\n");
+    return NULL;
+  }
+
+  hue_dtls_context *context = hue_dtls_context_create();
+  if (!context) {
+    fprintf(stderr, "hue_dtls_context_create() failed\n");
+    return NULL;
+  }
+
+  if (hue_dtls_connect(context, bridge_ip)) {
+    fprintf(stderr, "hue_dtls_connect() failed\n");
+    hue_dtls_context_free(context);
+    return NULL;
+  }
+
+  return context;
+}
+
+static void display_menu() {
+  while (true) {
+    printf("--------------------------------\n");
+    printf("1. THX Deep Note\n");
+    printf("2. Spider-Man: Into the Spider-Verse\n");
+    printf("3. Quit\n");
+    printf("--------------------------------\n");
+
+    printf("Enter your choice: ");
+    int choice = 0;
+    scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        return;
+      default:
+        printf("Invalid choice. Please try again.\n");
+        break;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -67,28 +111,22 @@ int main(int argc, char *argv[]) {
   const char *bridge_ip = argv[1];
 
   // Connect to the Hue bridge.
-  hue_dtls_context *context = hue_dtls_context_create();
-  if (!context) {
-    fprintf(stderr, "Failed to create DTLS context\n");
-    return 1;
-  }
-
   printf("Connecting to Hue bridge\n");
-
-  if (hue_dtls_connect(context, bridge_ip)) {
+  hue_dtls_context *context = connect_to_bridge(bridge_ip);
+  if (!context) {
     fprintf(stderr, "Failed to connect to Hue bridge\n");
-    hue_dtls_context_free(context);
     return 1;
   }
-
   printf("Connected to Hue bridge\n");
 
-  // Stream frames to the Hue bridge.
+  // Initialize the current frame mutex.
   if (pthread_mutex_init(&curent_frame_mutex, NULL)) {
     fprintf(stderr, "pthread_mutext_init() failed\n");
     hue_dtls_context_free(context);
     return 1;
   }
+
+  // Stream frames to the Hue bridge.
   pthread_t stream_thread = 0;
   stream_thread_args args = {context};
   if (pthread_create(&stream_thread, NULL, stream, &args)) {
@@ -98,12 +136,8 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  for (int i = 0; i < 30; i++) {
-    pthread_mutex_lock(&curent_frame_mutex);
-    current_frame[0].color_value[0] = 0xFFFF * i / 30;
-    pthread_mutex_unlock(&curent_frame_mutex);
-    sleep(1);
-  }
+  // Display animation menu.
+  display_menu();
 
   // Stop streaming.
   streaming = false;
@@ -120,4 +154,5 @@ int main(int argc, char *argv[]) {
  * 5. Implement the animations on the main thread at 60 Hz.
  * 6. Stop the animation on a Ctrl+C interrupt signal, return to the menu.
  * 7. Quit or Ctrl+D on the menu stops the sender thread and exits the program.
+ * 8. Curl to send start header.
  */
