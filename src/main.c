d57162a96243089a396242688943dd93326249be
Darik Harter <darik.harter@gmail.com>
2024-10-13 19:55:23 -0600

    Spider-Man: Replace seed with new best

--- src/main.c
+++ src/main.c
@@ -229,7 +229,7 @@ int main(int argc, char *argv[]) {
   // Seed the random number generator.
   const time_t seed = time(NULL);
   printf("seed: %ld\n", seed);
-  srand(1728867733);
+  srand(1728868986);
 
   // Display animation menu.
   display_menu();
